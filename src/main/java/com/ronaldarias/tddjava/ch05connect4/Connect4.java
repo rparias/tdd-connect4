65fb3f3e5d55bb627e14703cf3335b12ccfa99bd
Ronald Arias <ronald.arias@thoughtworks.com>
2020-07-26 14:07:49 -0500

    Requirement 2 - Players introduce discs on the top of the columns. An introduced disc drops down the board if the column is empty. Future discs introduced in the same column will stack over the previous ones

--- src/main/java/com/ronaldarias/tddjava/ch05connect4/Connect4.java
+++ src/main/java/com/ronaldarias/tddjava/ch05connect4/Connect4.java
@@ -1,19 +1,50 @@
 package com.ronaldarias.tddjava.ch05connect4;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Connect4 {
     private static final int COLUMNS = 7;
+    private static final int ROWS = 6;
+    private static final String EMPTY = " ";
 
-    public int getNumberOfDiscs() {
-        return 0;
+    private String[][] board = new String[ROWS][COLUMNS];
+
+    public Connect4() {
+        for (String[] row: board) {
+            Arrays.fill(row, EMPTY);
+        }
     }
 
-    public void putDiscInColumn(int column) {
+    public int getNumberOfDiscs() {
+        return IntStream.range(0, COLUMNS)
+                .map(this::getNumberOfDiscsInColumn).sum();
+    }
+
+    private int getNumberOfDiscsInColumn(int column) {
+        return (int) IntStream.range(0, ROWS)
+                .filter(row -> !EMPTY
+                        .equals(board[row][column]))
+                .count();
+    }
+
+    public int putDiscInColumn(int column) {
         checkColumn(column);
+        int row = getNumberOfDiscsInColumn(column);
+        checkPositionToInsert(row, column);
+        board[row][column] = "X";
+        return row;
     }
 
     private void checkColumn(int column) {
         if (column < 0 || column >= COLUMNS) {
             throw new RuntimeException("Invalid column " + column);
         }
+    }
+
+    private void checkPositionToInsert(int row, int column) {
+        if (row == ROWS)
+            throw new RuntimeException(
+                    "No more room in column " + column);
     }
 }
--- src/test/java/com/ronaldarias/tddjava/ch05connect4/Connect4Spec.java
+++ src/test/java/com/ronaldarias/tddjava/ch05connect4/Connect4Spec.java
@@ -32,4 +32,38 @@ public class Connect4Spec {
         exception.expectMessage("Invalid column " + column);
         tested.putDiscInColumn(column);
     }
+
+    @Test
+    public void whenFirstDiscInsertedInColumnThenPositionIsZero() {
+        int column = 1;
+        assertThat(tested.putDiscInColumn(column), is(0));
+    }
+
+    @Test
+    public void whenSecondDiscInsertedInColumnThenPositionIsOne() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.putDiscInColumn(column), is(1));
+    }
+
+    @Test
+    public void whenDiscInsertedThenNumberOfDiscsIncreases() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.getNumberOfDiscs(), is(1));
+    }
+
+    @Test
+    public void
+    whenNoMoreRoomInColumnThenRuntimeException() {
+        int column = 1;
+        int maxDiscsInColumn = 6; // the number of rows
+        for (int times = 0; times < maxDiscsInColumn; ++times) {
+            tested.putDiscInColumn(column);
+        }
+        exception.expect(RuntimeException.class);
+        exception.expectMessage("No more room in column " + column);
+        tested.putDiscInColumn(column);
+    }
+
 }
